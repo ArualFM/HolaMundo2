f069588f34f70bd6a2e0e3db642388808b6081e7
alumno <alumno@A301PC15>
2021-09-23 13:55:10 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo2/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace HolaMundo2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Esto no se toca porque deja de funcionar//
+        // Es el metodo que traduce el XAML en objetos ejecutables//
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MensajeTextBlock.Text = "Hola" + NombreTextBox.Text;
+            MensajeTextBlock.Foreground = Brushes.Blue;
+      ;   
+        }
+    }
+}
